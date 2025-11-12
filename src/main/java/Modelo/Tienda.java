e683e62ebce7ca2795bb45c3e26ef1ebf335ded0
Gurkan-000 <jr7rodri@gmail.com>
2025-11-12 06:45:40 -0500

    Agregando mas juegos

--- src/main/java/Modelo/Tienda.java
+++ src/main/java/Modelo/Tienda.java
@@ -8,8 +8,8 @@ package Modelo;
 public class Tienda {
     
     private int indice;
-    private String[] juego = {"Deep Rock","Counter Strike","Rust","Sky"};
-    private double[] precio = {33.0,5.0,25.0,44.0};
+    private String[] juego = {"Deep Rock","Counter Strike","Rust","Sky","R.E.P.O","Palword","Peak"};
+    private double[] precio = {33.0,5.0,25.0,44.0,15.0,63.0,11};
 
     public String getJuego(){
         return juego[indice];
